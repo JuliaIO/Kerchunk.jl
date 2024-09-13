ab751c476112c1d234b3a3dd4fdb9627f46a870e
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-09-13 11:46:30 -0700

    Document the CF correction interface

--- src/cf_corrections.jl
+++ src/cf_corrections.jl
@@ -1,10 +1,55 @@
-# TODO: don't use this function, since some CF convention tags
-# don't work with it.
-# Instead simply load via YAXArrays, which does the work correctly.
-# For some reason - Rasters.jl doesn't apply the CF conventions
-# (or maybe it's ZarrDatasets.jl's problem).
+#=
+# CF corrections
 
-function do_correction!(f!, store, path)
+The [Climate and Forecast Conventions](https://cfconventions.org/conventions.html) 
+("CF" conventions) are a _very_ popular way to encode geospatial metadata.  
+
+Notably, NetCDF (`.nc`), GeoTiff and GRIB files all follow this metadata format.
+
+The way CF conventions are framed is not quite compatible with Zarr's more modular structure
+of filters and compressors, instead each attribute has its own effect and can depend on the 
+value or presence of other attributes!  
+
+Kerchunk (and to some extent, Virtualizarr) aim to be more in line with the Zarr spec, but
+pure Zarr readers cannot read many CF encoded datasets correctly.  While this works with
+Xarray since it reads the CF metadata and applies it, other Zarr implementations do not
+have this luxury.  So, we implement some corrections that make life easier on the Zarr
+end, by which the values we read out of the data would be the same values one would read
+from the associated NetCDF file.
+
+=#
+
+@static if :FixedScaleOffsetFilter in names(Zarr; all = true)
+    import Zarr: FixedScaleOffsetFilter
+end
+
+"""
+    do_correction!(f!, store, path)
+
+Applies `f!` on the parsed `.zarray` and `.zattrs` files about the array 
+at path `path` in the Zarr store `store`.  These corrections mutate the 
+files `.zarray` and `.zmetadata`, and attempt to save them to the store.
+
+Available corrections are [`add_scale_offset_filter_and_set_mask!`](@ref) and
+[`move_compressor_from_filters!`](@ref).
+
+TODOs:
+- Make this work for consolidated metadata (check for the presence of a .zmetadata key)?
+
+## Usage
+
+````julia
+st,  = Zarr.storefromstring("reference://catalog.json")
+Kerchunk.do_correction!(Kerchunk.add_scale_offset_filter_and_set_mask!, st, "SomeVariable")
+zopen(st)
+````
+"""
+function do_correction!(f!, store::ReferenceStore, path)
+    
+    if !Zarr.is_zarray(store, path)
+        error("Kerchunk: $path in $store is not a `zarray`!  Halting correction.")
+    end
+
     zarray = Zarr.JSON.parse(store[path * "/.zarray"])
     zattrs = Zarr.JSON.parse(store[path * "/.zattrs"])
 
@@ -19,6 +64,38 @@ function do_correction!(f!, store, path)
     return
 end
 
+
+function do_correction!(f!, store::Zarr.AbstractStore, path)
+
+    if !Zarr.is_zarray(store, path)
+        error("Kerchunk: $path in $store is not a `zarray`!  Halting correction.")
+    end
+    
+    zarray = Zarr.JSON.parse(store[path * "/.zarray"])
+    zattrs = Zarr.JSON.parse(store[path * "/.zattrs"])
+
+    f!(zarray, zattrs)
+
+    # Write directly to the store, this is a lossy operation!!
+    store[path * "/.zarray"] = Zarr.JSON.json(zarray)
+    store[path * "/.zattrs"] = Zarr.JSON.json(zattrs)
+
+    return
+end
+
+"""
+    add_scale_offset_filter_and_set_mask!(zarray::Dict, zattrs::Dict)
+
+Adapts the CF metadata convention of scale/offset, valid_range, _FillValue,
+and _Unsigned by modifying the Zarr metadata to add:
+- An additional reinterpretation filter is added to the filter stack
+  if `_Unsigned=true`.  This allows the values to be interpreted as 
+  UInts instead of Ints, which removes the sign error that would otherwise
+  plague your dataset.
+- A `FixedScaleOffset` filter replaces `scale_factor` and `add_offset`.
+- `valid_range` and `_FillValue` are mutated based on the scale factor and added offset,
+  and the native Zarr `fill_value` is replaced by the mutated and read `_FillValue`.
+"""
 function add_scale_offset_filter_and_set_mask!(zarray::Dict, zattrs::Dict)
     scale = get(zattrs, "scale_factor", 1.0)
     offset = get(zattrs, "add_offset", 0.0)
@@ -28,7 +105,7 @@ function add_scale_offset_filter_and_set_mask!(zarray::Dict, zattrs::Dict)
     pop!(zattrs, "scale_factor")
     pop!(zattrs, "add_offset")
     filter_dict = Zarr.JSON.lower(
-        Zarr.FixedScaleOffsetFilter{
+        FixedScaleOffsetFilter{
             Float64,
             Zarr.typestr(zarray["dtype"]), 
             Zarr.typestr(get(zarray, "astype", "<f8"))
@@ -65,34 +142,32 @@ function add_scale_offset_filter_and_set_mask!(zarray::Dict, zattrs::Dict)
     end
 end
 
-function set_unsigned!(zarray::Dict{Symbol, <: Any}, zattrs::Dict{Symbol, <: Any})
-    if haskey(zattrs, "_Unsigned") && zattrs["_Unsigned"] == "true"
-        #=
-        filter_dict = Zarr.JSON.lower(Zarr.AsTypeFilter{}())
-        current_filters = zarray["filters"]
-        new_filters = if isnothing(current_filters) || isempty(current_filters)
-            [filter_dict]
+"""
+    move_compressor_from_filters!(zarray, zattrs)
+
+Checks if the last entry of `zarray["filters"]` is actually a compressor,
+and if there is no other compressor moves it from the filter array to the 
+`zarray["compressor"]` field.
+
+This is a common issue with Kerchunk metadata, since it seems numcodecs doesn't
+distinguish between compressors and filters.  This function will not be needed
+for Zarr v3 datasets, since the compressors and filters are all codecs in that 
+schema.
+"""
+function move_compressor_from_filters!(zarray::Dict{Symbol, <: Any}, zattrs::Dict{Symbol, <: Any})
+    if get(zarray, "filters", nothing) |> isnothing
+        return # No filters, so nothing to be done here
+    else # there are some filters
+        if !isnothing(zarray["compressors"])
+            return # there is already a compressor, we can't have multiple
         else
-            pushfirst!(collect(current_filters), filter_dict)
+            if last(zarray["filters"]).id in keys(Zarr.compressortypes)
+                compressor = pop!(zarray["filters"])
+                zarray["compressor"] = compressor
+                return
+            else # the last filter, first to be applied when decoding, is **not** a compressor.
+                return
+            end
         end
-        zarray["filters"] = new_filters
-        =#
-        old_dtype = Zarr.typestr(zarray["dtype"])
-        zarray["dtype"] = replace(zarray["dtype"], "i" => "u")
-        zarray["fillvalue"] = reinterpret(unsigned(old_dtype), old_dtype(zarray["fillvalue"]))
-    end
-end
-
-function move_filter_to_compressors!(zarray::Dict{Symbol, <: Any}, zattrs::Dict{Symbol, <: Any})
-end
-
-function cache_group!(store::ReferenceStore, group::String)
-    mkpath(joinpath(store.cache_dir, group))
-    for key in (group * "/" .* Zarr.subkeys(store, group))
-        cached_file_path = joinpath(store.cache_dir, key)
-        touch(cached_file_path)
-        bytes = _get_file_bytes(store, store[key])
-        write(cached_file_path, bytes)
-        store.cache[key] = (cached_file_path,)
     end
 end
