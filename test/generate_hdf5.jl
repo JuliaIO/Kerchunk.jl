e4b0418633dda4737a742ee09bbb06a3aa51d147
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-10-11 16:47:36 -0700

    add HDF5 test generator

---
+++ test/generate_hdf5.jl
@@ -0,0 +1,18 @@
+using HDF5
+using H5Zblosc, H5Zzstd, H5Zlz4
+# Generate a random array
+A = rand(100,100)
+
+compressors = (;
+    zstd = (; filters = [ZstdFilter(3)]),
+    blosc = (; filters = [BloscFilter(; level=9, shuffle=H5Zblosc.BITSHUFFLE, compressor="blosclz")]),
+    lz4 = (; filters = [Lz4Filter(0)]),
+    # bitshuffle = (; filters = [BitshuffleFilter(; shuffle=H5Zbitshuffle.NOSHUFFLE)]),
+)
+
+for (key, kwargs) in pairs(compressors)
+    h5open(joinpath("data", "test_$key.h5"), "w") do f
+        g = create_dataset(f, "A", HDF5.datatype(A), size(A); chunks = (20, 20), kwargs...)
+        g[:, :] = A
+    end
+end
